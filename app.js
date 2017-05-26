1956558b5d08aaded33d19ebee918130dc3bec89
Eli Oshinsky <elimelech.oshinsky@gmail.com>
2017-05-26 01:22:34 -0400

    updated mongo

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // connect to mongo db
-mongoose.connect('mongodb://localhost:27017/subscribers');
+mongoose.connect('mongodb://depthPerception:C@s1aw@y@ds013946.mlab.com:13946/emailsdepthperception');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
